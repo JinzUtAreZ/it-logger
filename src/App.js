92aac1c9ac3d18d1a5f890b07f66477bb261c3ba
Mathew C. Uy <mathew_uy@harbourcentre.com.ph>
2019-06-26 15:50:54 +0800

    Without REDUX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/css/materialize.min.js';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
   useEffect(() => {
@@ -14,6 +19,11 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
